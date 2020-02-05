5c3f79f097ebf5c4f5d00dbd6aade79bd3c27cb3
Yariv Nissim <yari.nis@me.com>
2020-02-04 19:38:18 -0800

    Initial Commit

---
+++ Upcoming Events/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Upcoming Events
+//
+//  Created by Yariv on 2/4/20.
+//  Copyright © 2020 Yariv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
